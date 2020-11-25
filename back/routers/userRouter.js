b0554622b8d9693cb3d3150a399b07e5c1cbe806
Andrys <andrys.magar@hotmail.fr>
2020-11-25 16:08:25 +0100

    bouton update  fonctionnelle , il faut bien afficher les message success et error

--- back/routers/userRouter.js
+++ back/routers/userRouter.js
@@ -3,7 +3,7 @@ import expressAsyncHandler from 'express-async-handler';
 import bcrypt from 'bcryptjs'
 import data from '../data.js';
 import User from '../models/usersModel.js';
-import { generateToken } from '../utils.js';
+import { generateToken, isAuth } from '../utils.js';
 
 const userRouter = express.Router();
 
@@ -56,6 +56,25 @@ userRouter.get('/:id', expressAsyncHandler(async (req, res) =>{
     } else {
         res.status(404).send( { message: 'Utilisateur Non Trouvé'});
         }
+    })
+);
+userRouter.put('/profile', isAuth, expressAsyncHandler(async(req,res) => {
+    const user = await User.findById(req.user._id);
+    if(user) {
+        user.name = req.body.name || user.name;
+        user.email = req.body.email || user.email;
+        if(req.body.password) {
+            user.password = bcrypt.hashSync(req.body.password, 8);
+        }
+        const updateUser = await  user.save();
+        res.send({
+            _id: updateUser._id,
+            name: updateUser.name,
+            email: updateUser.email,
+            isAdmin: updateUser.isAdmin,
+            token: generateToken(updateUser),
+        });
+    }
     })
 );
 export default userRouter;
