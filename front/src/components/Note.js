765b305d02962bfa7a2776885e021e5e10e4d2d3
Dryss <andrys.magar@hotmail.fr>
2020-11-26 03:26:39 +0100

    on recommence

---
+++ front/src/components/Note.js
@@ -0,0 +1,65 @@
+import React from 'react';
+
+export default function note(props) {
+  const { note, numReviews } = props;
+  return (
+    <div className="note">
+      <span>
+        <i
+          className={
+            note >= 1
+              ? 'fa fa-star'
+              : note >= 0.5
+              ? 'fa fa-star-half-o'
+              : 'fa fa-star-o'
+          }
+        ></i>
+      </span>
+      <span>
+        <i
+          className={
+            note >= 2
+              ? 'fa fa-star'
+              : note >= 1.5
+              ? 'fa fa-star-half-o'
+              : 'fa fa-star-o'
+          }
+        ></i>
+      </span>
+      <span>
+        <i
+          className={
+            note >= 3
+              ? 'fa fa-star'
+              : note >= 2.5
+              ? 'fa fa-star-half-o'
+              : 'fa fa-star-o'
+          }
+        ></i>
+      </span>
+      <span>
+        <i
+          className={
+            note >= 4
+              ? 'fa fa-star'
+              : note >= 3.5
+              ? 'fa fa-star-half-o'
+              : 'fa fa-star-o'
+          }
+        ></i>
+      </span>
+      <span>
+        <i
+          className={
+            note >= 5
+              ? 'fa fa-star'
+              : note >= 4.5
+              ? 'fa fa-star-half-o'
+              : 'fa fa-star-o'
+          }
+        ></i>
+      </span>
+      <span>{numReviews + ' reviews'}</span>
+    </div>
+  );
+}
